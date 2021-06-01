321056f912e882f94505b30a2d4ffca58e4e2b66
edmundloo <edmundloo92@gmail.com>
2021-06-01 15:31:48 +0800

    created product and productcolor entity

---
+++ src/main/java/org/generation/deployproduct/repository/Entity/Product.java
@@ -0,0 +1,47 @@
+package org.generation.deployproduct.repository.Entity;
+
+public class Product {
+
+    private Integer Product_code;
+    private String name;
+    private String description;
+    private String image_url;
+    private String category;
+    private double price;
+
+    public Product() {}
+
+    public Product(ProductDTO productDTO) {
+        this.name = productDTO.getName();
+        this.description = productDTO.getDescription();
+        this.image_url = productDTO.getImageUrl();
+        this.category = productDTO.getCategory();
+        this.price = productDTO.getPrice();
+    }
+
+    public Integer getProduct_code() { return this.Product_code; }
+    public void setProduct_code(Integer product_code) { this.Product_code = product_code; }
+
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return this.description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getImageUrl() { return this.image_url; }
+    public void setImage_url(String image_url) { this.image_url = image_url; }
+
+    public String getCategory() { return this.category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public double getPrice() { return this.price; }
+    public void setPrice(double price) { this.price = price; }
+
+
+    @Override
+    public String toString() {
+        return String.format("Product{Product_code='%d', name='%s', description='%s', image_url='%s', category='%s', price='%.2f'",
+                this.Product_code, this.name, this.description, this.image_url, this.category, this.price);
+    }
+
+}
---
+++ src/main/java/org/generation/deployproduct/repository/Entity/ProductColor.java
@@ -0,0 +1,26 @@
+package org.generation.deployproduct.repository.Entity;
+
+public class ProductColor {
+
+    private Integer Product_Product_code;
+    private String color;
+
+    public ProductColor() {}
+
+    public ProductColor(ProductColorDTO productColorDTO) {
+        this.color = productColorDTO.getColor();
+    }
+
+    public Integer getProduct_Product_code() { return this.Product_Product_code; }
+    public void setProduct_Product_code(Integer product_Product_code) { this.Product_Product_code = product_Product_code; }
+
+    public String getColor() { return this.color; }
+    public void setColor(String color) { this.color = color; }
+
+
+    @Override
+    public String toString() {
+        return String.format("ProductColor{Product_Product_code='%d', color='%s'", this.Product_Product_code, this.color);
+    }
+
+}
