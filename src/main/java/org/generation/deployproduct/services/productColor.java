9bf33762cb7ad9646ad9409aa1e4cd9acb854b71
fang1994-hub <layfang94@gmail.com>
2021-06-01 17:45:25 +0800

    Updated product color at services folder

--- src/main/java/org/generation/deployproduct/services/productColor.java
+++ src/main/java/org/generation/deployproduct/services/productColor.java
@@ -1,12 +1,15 @@
 package org.generation.deployproduct.services;
 
-import org.generation.deployproduct.repository.Entity.Item;
+import org.generation.deployproduct.repository.Entity.productColor;
 import java.util.List;
 
 public interface productColor {
 
     productColor save(productColor productcolor);
+
     void delete(int productcolorId);
+
     List<productColor> all();
+
     productColor findById(int productcolorId);
 }
