515d6a3370c95354aa4085c2953212ed5563fd98
TCLee-tech <81542713+TCLee-tech@users.noreply.github.com>
2021-06-01 17:55:38 +0800

    productDTO and productColorDTO
    01 June 2021

--- src/main/java/org/generation/deployproduct/controller/DTO/productColorDTO.java
+++ src/main/java/org/generation/deployproduct/controller/DTO/productColorDTO.java
@@ -1,39 +1,22 @@
-package org.generation.deployproduct.controller.DTO.
+package org.generation.deployproduct.controller.DTO;
 
-public class ItemDTO {
 
-    private String name;
-    private String description;
-    private String imageUrl;
-    private String style;
-    private double price;
+public class productColorDTO {
 
-    public ItemDTO(String name, String description, String imageUrl, String style, double price)
+    private int Product_Product_code;
+    private String color;
+
+    public productColorDTO(int Product_Product_code, String color)
     {
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.style = style;
-        this.price = price;
-    }
+        this.productCode = Product_Product_code;
+        this.color = color;
 
-    public String getName() {return name;}
+    public String getProductCode() {return Product_Product_code;}
 
-    public void setName(String name) {this.name = name; }
+    public void setProductCode(String ) {this.productCode = Product_Product_code; }
 
-    public String getDescription() {return description; }
+    public String getColor() {return color; }
 
-    public void setDescription(String description) {this.description = description;}
-
-    public String getImageUrl() {return imageUrl; }
-
-    public void setImageUrl(String imageUrl) {this.imageUrl = imageUrl; }
-
-    public void setStyle (String style) {this.style = style; }
-
-    public String getStyle() {return style; }
-
-    public void setPrice (double price) {this.price = price; }
-
-    public double getPrice() {return price; }
+    public void setColor(String color) {this.color = color;}
 }
+
