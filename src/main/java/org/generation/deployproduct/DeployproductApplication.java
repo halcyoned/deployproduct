e5c2dd03f2e5814afc728047244e15d9a74477d6
edmundloo <81542420+halcyoned@users.noreply.github.com>
2021-06-01 11:10:42 +0800

    initial publish

---
+++ src/main/java/org/generation/deployproduct/DeployproductApplication.java
@@ -0,0 +1,23 @@
+package org.generation.deployproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DeployproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployproductApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+	@GetMapping("/")
+	String hello() {
+		return "Hello World";
+	}
+}
---
+++ src/test/java/org/generation/deployproduct/DeployproductApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.deployproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
