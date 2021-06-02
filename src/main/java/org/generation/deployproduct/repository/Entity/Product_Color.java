b68dde531488b0dfd4f9fb0980c4cd7256260d78
edmundloo <edmundloo92@gmail.com>
2021-06-02 10:41:15 +0800

    updated entity classes
    added data-jpa and jdbc
    changed to localhost:8080

--- src/main/java/org/generation/deployproduct/repository/Entity/Product_Color.java
+++ src/main/java/org/generation/deployproduct/repository/Entity/Product_Color.java
@@ -2,16 +2,29 @@ package org.generation.deployproduct.repository.Entity;
 
 import org.generation.deployproduct.controller.DTO.ProductColorDTO;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product_Color {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Product_Color_id;
     private int Product_Product_code;
     private String color;
 
     public Product_Color() {}
 
     public Product_Color(ProductColorDTO productColorDTO) {
+        this.Product_Product_code = productColorDTO.getProduct_Product_code();
         this.color = productColorDTO.getColor();
     }
+
+    public int getProduct_Color_id() { return this.Product_Color_id; }
+    public void setProduct_Color_id(int product_Color_id) { this.Product_Color_id = product_Color_id; }
 
     public int getProduct_Product_code() { return this.Product_Product_code; }
     public void setProduct_Product_code(int product_Product_code) { this.Product_Product_code = product_Product_code; }
@@ -22,7 +35,8 @@ public class Product_Color {
 
     @Override
     public String toString() {
-        return String.format("ProductColor{Product_Product_code='%d', color='%s'", this.Product_Product_code, this.color);
+        return String.format("ProductColor{Product_Color_id='%d', Product_Product_code='%d', color='%s'",
+                this.Product_Color_id, this.Product_Product_code, this.color);
     }
 
 }
